1fdeb9a2741c1a67444147a7fe879c5f014eaec0
Dhairya Patel <dhairya.patel@somaiya.edu>
2020-06-24 04:11:15 +0530

    Initial

---
+++ frontend/src/data.js
@@ -0,0 +1,37 @@
+export default  {products:[
+   { name :'Slim Fit Shirt',
+    category:'Shirt',
+    image:'/images/d1.jpg',
+    brand:'Buffalo',
+    price:1200,
+    stars:4.5,
+    numReviews:10
+   },
+   {
+    name :'Fit Shirt',
+    category:'Shirt',
+    image:'/images/d1.jpg',
+    brand:'Buffalo',
+    price:'1200',
+    stars:'4.5',
+    numReviews:'10 Reviews',
+   },
+   {
+    name :'Trouser',
+    category:'Pant',
+    image:'/images/d1.jpg',
+    brand:'Buffalo',
+    price:'1200',
+    stars:'4.5',
+    numReviews:'10 Reviews',
+   },
+   {
+    name :'Jeans',
+    category:'Pant',
+    image:'/images/d1.jpg',
+    brand:'Buffalo',
+    price:'1200',
+    stars:'4.5',
+    numReviews:'10 Reviews'
+   }
+]}
